80c297c02b757b7bf6c812d04f76b92a0d3bc9cd
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2024-12-25 00:01:19 +0100

    Delete How-to-Implement-a-Multi-Religion-Electoral-System-A-Step-by-Step-Guide/The Faith Representatives Chamber (FRC_files directory

--- How-to-Implement-a-Multi-Religion-Electoral-System-A-Step-by-Step-Guide/The Faith Representatives Chamber (FRC_files/a_data_004/cBmLqZhej__.js
+++
@@ -1,4 +0,0 @@
-;/*FB_PKG_DELIM*/
-
-__d("IgWebPageViewFalcoEvent",["FalcoLoggerInternal","getFalcoLogPolicy_DO_NOT_USE"],(function(a,b,c,d,e,f,g){"use strict";a=c("getFalcoLogPolicy_DO_NOT_USE")("636");b=d("FalcoLoggerInternal").create("ig_web_page_view",a);e=b;g["default"]=e}),98);
-__d("XPlatReactNestedPressableContext",["react"],(function(a,b,c,d,e,f,g){"use strict";var h;a=h||d("react");b=a.createContext(!1);g["default"]=b}),98);
